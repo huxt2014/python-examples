259b29a7cb9f9238dc299e2a0c55b510335b8b1b
cirobot <xuetao.hu@shatacloud.com>
2016-06-21 18:09:11 +0800

    sqlalchemy orm DML

--- sqlalchemy/ORM_DML.py
+++ sqlalchemy/ORM_DML.py
@@ -1,77 +1,62 @@
 
+
 # basic query ########################################
+#     The class Query is used for ORM-level SQL construction. It features a  
+# generative interface whereby successive calls return a new Query object, a 
+# copy of the former with additional criteria and options associated with it.
+#     session.query() takes a variable number of arguments, any combination of  
+# mapped class, a Mapper object, an orm-enabled descriptor, or an AliasedClass 
+# object. And then return the query instance.
+#     query.all() and other functions emit the SQL and return result. WHen query 
+# more than one entity, the result is expressed as named tuple.
 
-## select instance
-for u in  (Session.query(User)
-                  .filter_by(ID=1)                  # where clause
-                  .filter(User.ID == 1)             # and
-                  .filter(User.ID == 1,             # and
-                          User.ID == 1,
-                          User.ID == 1)
-                  .distinct()                       # distinct
-                  .order_by(User.ID)                # order by
-                  .all()):
-    print u
+## query
+session.query(User)                       # mapped class
+session.query(User.id, User.fullname)     # descriptor
+session.query(User, User.id)              # combination
 
-## select several columns
-for id, name in (Session.query(User.ID, 
-                               User.NAME.label('MY_NAME'))     # aliases
-                        .all()):
-    print id, name
-    
-## select instance and columns
-for user, id in (Session.query(User, User.ID)
-                        .all()):
-    print user, project_id
+## filter
+##     The criterion is any SQL expression object applicable to the WHERE clause
+## of a select.
+query = session.query(User)
+query.filter_by(id=1)                  # uses keyword arguments
+query.filter(User.id == 1)             # use SQL expression object
+query.filter(text("id<10"))            # use textual SQL
+(query.filter(text("id<:value and name=:name"))   # bind parameters
+      .params(value=10, name='hello'))
 
 ## get query result
-result = (Session.query(User)
-                 .all()         # list
-              #  .first()         instance or None
-              #  .one()           instance or NoResultFound or MultipleResultsFound
-              #  .one_or_none()   instance or None or MultipleResultsFound
-              #  .scalar()        instance or None
-              #  [1:3]            list with limit and offset
-              )
+result = Session.query(User)
 
-
-## using textual SQL
-from sqlalchemy import text
-
-resutl = (Session.query(User)
-                 .filter(text("id<10"))
-                 .filter(text("id<:value and name=:name")).params(value=10, name='hello')
-                 .all()) 
+result.all()             # fetch all as list
+result.first()           # fetch one or None
+result.one()             # fetch one or NoResultFound or MultipleResultsFound
+result.one_or_none()     # fetch one or None or MultipleResultsFound
+result.scalar()          # invokes the one() method and return the first column
+result[1:3]              # list with limit and offset
+for row in result:       # iterate all
+    pass
 
 
 
 # join  ###############################################
 
-## get one instance without duplicate
-for u in (Session.query(User)
-                 .join(Project, User.ID==Project.USER_ID)
-                 .all()):
-    print u
+## join implicitly
+(Session.query(User, Project)                     # get two instances
+        .filter(User.ID==Project.USER_ID))
 
-## get two instances
-for u, p in (Session.query(User, Project)
-                    .filter(User.ID==Project.USER_ID)
-                    .all()):
-    print u, p
+(Session.query(User.NAME, Project.NAME)           # get several columns
+        .filter(User.ID==Project.USER_ID))
 
-## get several columns
-for name, p_name in (Session.query(User.NAME, Project.NAME.label('PROJECT_NAME'))
-                            .filter(User.ID==Project.USER_ID)):
-    print name, p_name
+## join explicitly
+(Session.query(User)                              # without duplicate
+        .join(Project, User.ID==Project.USER_ID))
 
-## left outer Join
-for u, project_id in (Session.query(User, Project.ID.label('PROJECT_ID'))
-                             .outerjoin(Project, 
-                                        and_(User.ID==Project.USER_ID,
-                                             User.ID==Project.USER_ID))
-                             .all()):
-    print u, project_id
-
+(Session.query(User, Project.ID)                  # left outer Join
+        .outerjoin(Project, 
+                   and_(User.ID==Project.USER_ID,
+                        User.ID==Project.USER_ID)))
+                        
 
 
 # update ##############################################
@@ -110,31 +95,37 @@ Session.delete(user)
 
 
 
-# aggregation ############################################
+# other option
 
+## order by
+query.order_by(User.id)
+
+## distinct
+query.distinct()            
+
+## aggregation
 from sqlalchemy import func
 
-Session.query(User.ID.
+(Session.query(User.ID.
               func.sum(User.SALARY).albel('SALARY'),
-              func.count('*').label('TOTAL_NUMBER')
-       .group_by(User.ID)
-       .all())
-      
-
-
-# other ##################################################
+              func.count('*').label('TOTAL_NUMBER'))
+        .group_by(User.ID)
+        .all())
 
 ## case clause
 from sqlalchemy.sql.expression import case
 
-Session.query(User.ID,
-              case([(User.SALARY < 1000, 1),
-                    (User.SALARY.between(1000,2000), 2)],
-                   else_=0
-                   ).label('SALARY')
-              .all()
-              )
+(Session.query(User.ID,
+               case([(User.SALARY < 1000, 1),
+                     (User.SALARY.between(1000,2000), 2)],
+                    else_=0).label('SALARY'))
+        .all())
 
+## function
+from sqlalchemy.sql import func
+
+Session.query(func.now())       # select now()
+Session.query(func.any_func())  # select any_func()
 
 
 
